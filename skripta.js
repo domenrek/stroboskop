0f29991dfdbbc213bee8aac288d0befbd8d5561c
Domen Rek <dr8276@student.uni-lj.si>
2016-03-20 13:53:32 +0000

    Dinamika gumba za ustavitev stroboskopa

--- skripta.js
+++ skripta.js
@@ -31,20 +31,25 @@ window.addEventListener('load', function() {
 		if (ustavi) {
 			ustavi = false;
 		} else {
-			novId = (id+1) % vrednosti.length;
-			timeout = Math.floor((Math.random() * (maxCas-minCas)) + minCas);
+			var novId = (id+1) % vrednosti.length;
+			var timeout = Math.floor((Math.random() * (maxCas-minCas)) + minCas);
 			setTimeout(function() {spremeniBarvo(novId)} , timeout);
 		}		
 	}
 	
 	var stop = function(event) {
 		ustavi = true;
+		var stop = document.querySelector("#start");
+		stop.innerHTML = "Zaženi stroboskop";
+		stop.removeEventListener('click', stop);
+		stop.addEventListener('click', zagon);
 	}
 	
 	var zagon = function(event) {
+		ustavi = false;
 		vrednosti = [];
 		var barve = document.querySelectorAll("#barve > button");
-		for (i = 0; i < barve.length; i++) {
+		for (var i = 0; i < barve.length; i++) {
 			var barva = barve[i];
 			vrednosti.push(barva.innerHTML);
 		}
